d230310bc04cff541d727b85119ae14d0650af02
edgar muñoz manjón <edgarmunozmanjon@gmail.com>
2025-07-23 10:55:25 +0200

    prepare userService

--- src/api/appwrite.ts
+++ src/api/appwrite.ts
@@ -1,15 +1,12 @@
-// src/api/appwrite.ts
-import { Client, Account, Databases, ID, Storage } from 'appwrite';
+import { Client, Account, Databases, Query, ID } from 'appwrite';
 
 const client = new Client();
 
 client
-  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)  // Cambia esto por la URL de tu servidor Appwrite
-  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);                // Cambia esto por el ID de tu proyecto Appwrite
+  .setEndpoint(import.meta.env.VITE_APPWRITE_ENDPOINT)
+  .setProject(import.meta.env.VITE_APPWRITE_PROJECT_ID);
 
-export const account = new Account(client);
-export const databases = new Databases(client);
-export const storage = new Storage(client); //opcional
+const account = new Account(client);
+const databases = new Databases(client);
 
-// Exporta también ID si quieres usar IDs únicos para documentos
-export { ID };
+export { client, account, databases, Query, ID };
