8406591d175143355b6fa247f7e680adfe2ca235
edgar muñoz manjón <edgarmunozmanjon@gmail.com>
2025-07-27 11:32:49 +0200

    prepare custom hooks and contexts

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,19 @@
+import { createContext } from 'react';
+
+export type User = {
+  $id: string;
+  name: string;
+  email: string;
+};
+
+export type AuthContextType = {
+  user: User | null;
+  setUser: (user: User | null) => void;
+  logout: () => Promise<void>;
+};
+
+export const AuthContext = createContext<AuthContextType>({
+  user: null,
+  setUser: () => {},
+  logout: async () => {},
+});
